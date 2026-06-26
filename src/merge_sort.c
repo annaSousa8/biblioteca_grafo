a8bc1ffae944a067bb1f72cee07f7a939eda12b0
R-Vilela <raulvilela29@gmail.com>
2026-06-26 15:07:09 -0400

    fix(merge): merge 2 list to one on merge algorithm

--- src/merge_sort.c
+++ src/merge_sort.c
@@ -1,25 +1,29 @@
 #include "merge_sort.h"
 
 void merge_sort(int *lista, int inc, int fim){
+    int meio = (inc+fim)/2;
+
     if(inc!=fim){
-        int meio = (inc+fim)/2;
         merge_sort(lista, inc, meio);
         merge_sort(lista, meio+1, fim);
     }
 
-    merge(lista, inc, fim);
+    merge(lista, inc, meio, meio+1, fim);
 
 }
 
-void merge(int *lista, int inc, int fim){
-    int i=inc, j=fim+1;
-    while(i<=inc||j<=fim){
+void merge(int *lista, int inc_1,int fim_1, int inc_2, int fim_2){
+    int i=inc_1, j=inc_2;
+    while(i<=fim_2&&j<=fim_2){
+        if(i==j){
+            j++;
+            continue;
+        }
         if(lista[i]<lista[j]){
             swap(&(lista[i]), &(lista[j]));
-            j++;
-        }else{
             i++;
-        }
+        }else
+            i++;
     }
 }
 
