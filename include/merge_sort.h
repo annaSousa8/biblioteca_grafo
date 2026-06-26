07f551da9430f98a3e7446b420bcf3e4d606eedb
R-Vilela <raulvilela29@gmail.com>
2026-06-26 15:07:34 -0400

    squash with merge

--- include/merge_sort.h
+++ include/merge_sort.h
@@ -1,5 +1,5 @@
 void merge_sort(int *lista, int inc, int fim);
 
-void merge(int *lista, int inc, int fim);
+void merge(int *lista, int inc_1,int fim_1, int inc_2, int fim_2);
 
 void swap(int *i, int *j);
